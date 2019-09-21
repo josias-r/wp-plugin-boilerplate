a090dab62168e06f78143452ed48fa51b7f880e7
josias-r <josias.ribi@gmail.com>
2019-09-21 10:25:38 +0200

    installed linters

---
+++ src/blocks/guten-block.js
@@ -0,0 +1,30 @@
+const { registerBlockType } = wp.blocks;
+const { withSelect } = wp.data;
+
+registerBlockType("gutenberg-examples/example-05-dynamic", {
+  title: "Example: last post",
+  icon: "megaphone",
+  category: "widgets",
+
+  edit: withSelect(select => {
+    return {
+      posts: select("core").getEntityRecords("postType", "post")
+    };
+  })(({ posts, className }) => {
+    if (!posts) {
+      return "Loading...";
+    }
+
+    if (posts && posts.length === 0) {
+      return "No posts";
+    }
+
+    const post = posts[0];
+
+    return (
+      <a className={className} href={post.link}>
+        {post.title.rendered}
+      </a>
+    );
+  })
+});
